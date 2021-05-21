35cf343b14975496c591660a0479ae00e9dfef3d
Hon Lam <hon@junglescout.com>
2021-05-21 15:31:45 -0700

    Flesh out function for creating the printed query.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from 'url'
 
 import { pool } from './database_connection.js'
 
-PRINTED_ROWS_MAX = {
+const PRINTED_ROWS_MAX = {
   BEFORE: 2,
   AFTER: 2
 }
@@ -48,10 +48,7 @@ const getLeftPositions = ({ transpiledQuery, errorPosition }) => {
   return { errorLeftPosition, newLinePositions }
 }
 
-const getKaratLine = ({ newLinePositions, errorLeftPosition, errorPosition}) => {
-
-  // keep this ----------------------
-
+const getKaratLine = ({ newLinePositions, errorLeftPosition, errorPosition }) => {
   let karatLeftOffset = 0
   if (!newLinePositions.includes(errorPosition)) {
     karatLeftOffset = errorPosition - errorLeftPosition
@@ -60,8 +57,52 @@ const getKaratLine = ({ newLinePositions, errorLeftPosition, errorPosition}) => 
   return '▲'.padStart(karatLeftOffset, ' ')
 }
 
-const getPrintedQuery = ({ transpiledQuery, newLinePositions, karatLine }) => {
+const getPrintedQuery = ({ transpiledQuery, newLinePositions, karatLine, errorLeftPosition }) => {
   const errorLineIndex = newLinePositions.indexOf(errorLeftPosition)
+
+  const printPositions = {
+    first: null,
+    leftAfterError: null,
+    last: null
+  }
+
+  let firstLineIndex = errorLineIndex - PRINTED_ROWS_MAX.BEFORE
+  if (firstLineIndex < 0) {
+    firstLineIndex = 0
+  }
+
+  printPositions.first = newLinePositions[firstLineIndex]
+
+  printPositions.leftAfterError = newLinePositions[errorLineIndex + 1]
+  if (!printPositions.leftAfterError) {
+    printPositions.leftAfterError = transpiledQuery.length
+  }
+
+  let lastLineIndex = errorLineIndex + PRINTED_ROWS_MAX.AFTER
+  if (lastLineIndex >= newLinePositions.length) {
+    lastLineIndex = newLinePositions.length - 1
+  }
+
+  let leftPositionAfterLastLine = newLinePositions[lastLineIndex + 1]
+
+  printPositions.last = transpiledQuery.length
+  if (leftPositionAfterLastLine) {
+    printPositions.last = leftPositionAfterLastLine - 1
+  }
+
+  return transpiledQuery.split('').map((character, index) => {
+    const position = index + 1
+
+    if (position < printPositions.first || position > printPositions.last) {
+      return ''
+    }
+
+    if (position == printPositions.leftAfterError) {
+      return '\n' + karatLine + character
+    } else {
+      return character
+    }
+  }).join('')
 }
 
 const writeToFile = transpiledQuery => {
@@ -98,17 +139,27 @@ const handleError = ({
 
   const errorPosition = error.position
 
-  const karatLine = getKaratLine({
-    transpiledQuery,
-    errorPosition
-  })
-
   const { errorLeftPosition, newLinePositions } = getLeftPositions({
     transpiledQuery,
     errorPosition
   })
 
-  writeToFile(transpiledQuery)
+  const karatLine = getKaratLine({
+    newLinePositions,
+    errorLeftPosition,
+    errorPosition
+  })
+
+  const printedQuery = getPrintedQuery({
+    transpiledQuery,
+    newLinePositions,
+    karatLine,
+    errorLeftPosition
+  })
+
+  console.log(printedQuery)
+
+  // writeToFile(transpiledQuery)
 }
 
 // run this in Node (or REPL) console ----
